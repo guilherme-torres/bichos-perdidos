e08ea088fb86633f1b2a897e8b50ee84f79ced07
guilherme-torres <guilhermetorres1401@gmail.com>
2024-05-14 11:56:05 -0300

    criar rota de salvar dados no bd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const mysql = require("mysql2");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -10,7 +11,15 @@ const storage = multer.diskStorage({
         cb(null, uniqueSuffix + "-" + file.originalname);
     }
 });
-  
+
+const conn = mysql.createConnection({
+    host: "172.20.0.1",
+    port: 3306,
+    user: "root",
+    password: "PASSWORD",
+    database: "bichos_perdidos"
+});
+
 const upload = multer({ storage: storage });
 
 const app = express();
@@ -21,7 +30,17 @@ app.use("/uploads", express.static("uploads"));
 app.post("/animal", upload.single("animalPhoto"), (req, res) => {
     const photoUrl = "http://localhost:3333/uploads/" + req.file.filename;
     const { phoneNumber, city, uf, info } = req.body;
-    return res.json({ photoUrl, phoneNumber, city, uf, info });
+
+    const sql = "INSERT INTO `anima` (`photo_url`, `phone_number`, `city`, `uf`, `info`) VALUES (?, ?, ?, ?, ?)";
+    const values = [photoUrl, phoneNumber, city, uf, info];
+
+    conn.execute(sql, values, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(502);
+        }
+        return res.status(201).json({ photoUrl, phoneNumber, city, uf, info });
+    });
 });
 
 app.listen(3333);
