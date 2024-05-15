ddc198b0b0fe4c17c7720b32c0cc5249416a5bc8
guilherme-torres <guilhermetorres1401@gmail.com>
2024-05-15 10:17:56 -0300

    criar tabela no bd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const routes = require("./routes");
+const conn = require("./db")
+
+const sql = `
+CREATE TABLE IF NOT EXISTS animal (
+    id int not null auto_increment primary key,
+    photo_url text not null,
+    phone_number varchar(25) not null,
+    city varchar(100) not null,
+    uf char(2) not null,
+    info text
+)`;
+conn.query(sql, (err) => {
+    if (err) throw err;
+    console.log("Table created");
+});
 
 const app = express();
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path")
-const AnimalController = require("./controllers/AnimalController");
+const path = require("path");
+const AnimalController = require("./controllers/animalController");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
